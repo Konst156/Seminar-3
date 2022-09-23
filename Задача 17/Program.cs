ea2573207747c58b2c8ec369780b072c17c978e9
Konstantin <kzhestovskih2012@gmail.com>
2022-09-23 17:44:43 +0500

    Семинар 3

---
+++ Задача 17/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.Clear();
+Console.WriteLine("Введите номер четверти");
+int N = int.Parse(Console.ReadLine());
+
+if (N ==1)
+{
+    Console.WriteLine("X>0 и Y>0");
+}
+if(N == 2)
+{
+    Console.WriteLine("X<0 и Y>0");
+}
+if(N == 3)
+{
+    Console.WriteLine("X<0 и Y<0");
+}
+if(N == 4)
+{
+    Console.WriteLine("X>0 и Y<0");
+}
---
+++ задача 18/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// AB = √(xb - xa)2 + (yb - ya)2 
+// Math.Sqrt()
+// Math.Pow()
+
+Console.Clear();
+Console.Write("Введите X точки A: ");
+double XA = double.Parse(Console.ReadLine());
+Console.Write("Введите Y точки A: ");
+double YA = double.Parse(Console.ReadLine());
+Console.Write("Введите X точки B: ");
+double XB = double.Parse(Console.ReadLine());
+Console.Write("Введите Y точки B: ");
+double YB = double.Parse(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow((XB - XA),2) + Math.Pow((YB - YA), 2));
+
+Console.Write($"растояние между двумя точками = {AB}");
+
+
